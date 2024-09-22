d87956d72c7775dc605aab02bcdb9c8297c7228b
BagrijRoman <bagrijroman@gmail.com>
2024-09-22 16:07:33 +0300

    add helmet for securitu reasons

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 import { httpLogger, verifyJWT } from './middlewares';
 import { logger, initSwaggerDocs } from './utils';
@@ -13,11 +14,11 @@ const app = express();
 const port = Number(process.env.PORT || 4000);
 
 app.use(cors({ origin: process.env.CLIENT_DOMAIN }));
-app.use(httpLogger);
+app.use(helmet());
 app.use(express.json());
-app.use(verifyJWT);
 
-// todo add helmet !!!!!!!!
+app.use(httpLogger);
+app.use(verifyJWT);
 
 app.use('/test', testRouter);
 app.use('/auth', authRouter);
