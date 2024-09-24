8bfb54b41edf0f0423d08c5ab2d48116cc0c8a85
BagrijRoman <bagrijroman@gmail.com>
2024-09-24 19:38:25 +0300

    add auth check on startup

---
+++ clien/src/Router.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import { Home, Login, NotFound, User, UsersList } from "./pages";
+import { RequireAuth } from "./hoc/RequireAuth";
+import { ApiService } from './services';
+import { useAuth } from './hook/useAuth';
+
+export const Router = () => {
+  const { user, signIn } = useAuth();
+
+  const checkAuth = async () => {
+    if (!user) {
+      const { error, user: apiUser } = await ApiService.checkAuthOnStartup();
+
+      if (apiUser && !error) {
+        signIn(apiUser);
+      }
+    }
+  }
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="users" element={
+          <RequireAuth>
+            <UsersList />
+          </RequireAuth>
+        } />
+        <Route path="user/:id" element={
+          <RequireAuth>
+            <User />
+          </RequireAuth>
+        } />
+
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
