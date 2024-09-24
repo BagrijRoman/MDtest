e4bd34a52a18f2e911c15a6e8bb60e996a0c551c
BagrijRoman <bagrijroman@gmail.com>
2024-09-24 12:15:01 +0300

    add update user profile API

--- api/src/const/validationRules.ts
+++ api/src/const/validationRules.ts
@@ -3,6 +3,13 @@ export const VALIDATOR_RULES = {
   password: "required|minLength:5|maxLength:50",
   firstName: "required|minLength:3|maxLength:50",
   lastName: "required|minLength:3|maxLength:50",
+  optionalName: "minLength:3|maxLength:50",
+  middleName: "minLength:3|maxLength:50",
+
+  stringId: "required|minLength:24|maxLength:24",
+
+  address: "minLength:3|maxLength:200",
+  extraInfo: "minLength:3|maxLength:200",
 
   offset: "required|min:0",
   limit: "required|min:1|max:100",
